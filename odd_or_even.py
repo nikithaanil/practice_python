28cc7f8d4865121ceff0063a817eba62948c2630
nikithaanil <nikithaanil@gmail.com>
2018-01-26 12:45:17 +0530

    25/01/18

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,11 +1,13 @@
-#odd or even
-
+'''Ask the user for a number.
+Depending on whether the number is even or odd, print out an appropriate message to the user
+'''
 num=int(input("enter the number to be checked: "))
 result=num%2
+
 if result == 0:
     print("{} is an even number".format(num))
 else:
     print("{} is an odd number".format(num))
 
-if num%4==0:
-    print("{} is a multiple of 4".format(num))
+#if num%4==0:
+#    print("{} is a multiple of 4".format(num))
