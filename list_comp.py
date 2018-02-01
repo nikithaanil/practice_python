39e821dd4208403a20f597ba5edb40e46ec62c72
nikithaanil <nikithaanil@gmail.com>
2018-02-01 21:38:34 +0530

    01/02/18

---
+++ list_comp.py
@@ -0,0 +1,17 @@
+'''input a list and makes a new list that has only the even elements of this list in it.'''
+n=int(input("\nenter the limit: "))
+a=[]
+newlist=[]
+#print("\nenter the elements : ")
+'''
+for i in range(n):
+    a.append(int(input()))
+print(a)
+
+for x in a:
+    if x % 2 == 0:
+        newlist.append(x)
+print("even numbered list: ")
+print(newlist)
+'''
+#print([newlist.append(for i in a:if a%2==0:)])
